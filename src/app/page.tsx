4088d2ad23a8fa2acc69ccbd1f5b815f0f842746
Khen Channmakara <khenchannmakara@gmail.com>
2024-12-07 21:38:28 +0700

    add dynamci import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 import Head from "next/head";
-import ContactForm from "../components/ContactForm";
-import Education from "../components/Education";
-import Effect from "../components/Effect";
-import HeroSections from "../components/HeroSections";
-import ProjectSection from "../components/Project";
-import Skills from "../components/Skills";
-import ScrollToTop from "../components/ScrollToTop";
+import dynamic from "next/dynamic";
+const ContactForm = dynamic(() => import("../components/ContactForm"));
+const Education = dynamic(() => import("../components/Education"));
+const Effect = dynamic(() => import("../components/Effect"));
+const HeroSections = dynamic(() => import("../components/HeroSections"));
+const ProjectSection = dynamic(() => import("../components/Project"));
+const Skills = dynamic(() => import("../components/Skills"));
+const ScrollToTop = dynamic(() => import("../components/ScrollToTop"));
 
 export default function Home() {
   return (
     <main>
       <Head>
-        <title>My Portfolio</title>
+        <title>Makara-Portfolio</title>
         <meta
           name="description"
           content="Welcome to my portfolio website. Here you can see my work, skills, education, and contact information."
